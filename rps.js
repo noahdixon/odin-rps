7bad40dee7eec5c5982a8b94bd53ade6dbf008b8
Noah Dixon <114971241+noahdixon@users.noreply.github.com>
2023-02-04 01:00:19 -0500

    Implement all game functionality except restart

--- rps.js
+++ rps.js
@@ -47,17 +47,17 @@ function playRound(playerSelection, computerSelection) {
     const result = outcomes[selectionValue(playerSelectionCased)][selectionValue(computerSelection)];
 
     if (result === 'Win') {
-        return 'You win! ' + playerSelectionCased + ' beats ' + computerSelection + '.';
+        return 'Round won! ' + playerSelectionCased + ' beats ' + computerSelection + '.';
     } 
     
     if (result === 'Lose') {
-        return 'You lose! ' + computerSelection + ' beats ' + playerSelectionCased + '.';
+        return 'Round lost! ' + computerSelection + ' beats ' + playerSelectionCased + '.';
     }
 
-    return 'You draw! ' + playerSelectionCased + ' ties ' + computerSelection + '.';
+    return 'Round draw! ' + playerSelectionCased + ' ties ' + computerSelection + '.';
   }
 
-  /**
+/**
  * Simulates a five round game of rock, paper, scissors.
  *
  * @return {string} The result of the game.
@@ -101,3 +101,99 @@ function game() {
      }
 
 }
+
+/**
+ * Reverts all buttons in a NodeList of buttons to their
+ * default size and color.
+ *
+ * @param {NodeList} buttons The button's to revert.
+ */
+function revertButtons(buttons) {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = 'rgb(199, 236, 255)';
+        button.style.scale = 1;
+    });
+}
+
+/**
+ * Changes the size and color of the CPU buttons based on the CPU's selection.
+ *
+ * @param {string} cpuChoice The CPU's move.
+ */
+ function changeCpuButtonColor(cpuChoice) {
+    revertButtons(cpuButtons);
+    cpuButtons.forEach((button) => {
+        if(cpuChoice.toLowerCase() === button.id.substring(4)) {
+            button.style.backgroundColor = 'rgb(149, 220, 255)';
+            button.style.scale = 1.25;
+        }
+    })
+}
+
+/**
+ * Updates the player and CPU scores based on the result of a round.
+ * Increments either the player's or CPU's score by one, or does nothing if the round is a draw.
+ *
+ * @param {string} roundResult The result of one round.
+ */
+function updateScores(roundResult) {
+    if (roundResult.charAt(6) === 'w') {
+        playerScoreVal++;
+        playerScore.textContent = 'Score: ' + playerScoreVal;
+    } else if (roundResult.charAt(6) === 'l') {
+        cpuScoreVal++;
+        cpuScore.textContent = 'Score: ' + cpuScoreVal;
+    }
+}
+
+/**
+ * Updates the player and CPU scores based on the result of a round.
+ * Increments either the player's or CPU's score by one, or does nothing if the round is a draw.
+ *
+ * @param {string} roundResult The result of one round.
+ */
+function checkScore() {
+    if (playerScoreVal === 5) {
+        results.textContent = 'You won the game! Final score: ' + playerScoreVal + '-' + cpuScoreVal;
+        return true;
+    }
+    
+    if (cpuScoreVal === 5) {
+        results.textContent = 'You lost the game. Final score: ' + playerScoreVal + '-' + cpuScoreVal;
+        return true;
+    }
+
+    return false;
+}
+
+function playerClick (event) {
+    revertButtons(playerButtons);
+    event.currentTarget.style.backgroundColor = 'rgb(149, 220, 255)';
+    event.currentTarget.style.scale = 1.25;
+
+    cpuChoice = getComputerChoice();
+    changeCpuButtonColor(cpuChoice);
+
+    roundResult = playRound(event.currentTarget.id, cpuChoice);
+    updateScores(roundResult);
+    results.textContent = roundResult;
+
+    if(checkScore()) {
+        revertButtons(playerButtons);
+        revertButtons(cpuButtons);
+        playerButtons.forEach(button => button.removeEventListener('click', playerClick));
+    }
+
+
+}
+
+
+const playerButtons = document.querySelectorAll('.player-button');
+const cpuButtons = document.querySelectorAll('.cpu-button');
+const results = document.querySelector('#results');
+const playerScore = document.querySelector('#player-score');
+const cpuScore = document.querySelector('#cpu-score');
+let playerScoreVal = 0;
+let cpuScoreVal = 0;
+
+playerButtons.forEach(button => button.addEventListener('click', playerClick));
